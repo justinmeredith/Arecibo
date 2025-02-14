237437ec06df7490ea762281856d7e77e7998e50
justinmeredith <jm.4444@icloud.com>
2025-02-14 17:45:34 -0600

    Added the secret message generator, the main loop, and the main loop executor.

--- Arecibo.py
+++ Arecibo.py
@@ -3,7 +3,20 @@ import random
 message_length = 3
 guesses_allowed = 10
 
+def main():
+    print('The program is running.')
+    secret_message = generateSecretMessage()
+    print('The secret message is {}.'.format(secret_message))
+
 def generateSecretMessage():
     numbers = list('1234567890')
     random.shuffle(numbers)
-    
+
+    secret_message = ''
+
+    for i in range(message_length):
+        secret_message += str(numbers[i])
+    return secret_message
+
+if __name__ == '__main__':
+    main()
