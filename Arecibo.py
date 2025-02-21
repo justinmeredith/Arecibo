6fe0bc5c1c325e07e1533e54b1996c2c161e3fe9
justinmeredith <jm.4444@icloud.com>
2025-02-21 11:37:52 -0600

    Working on the loop for guesses.

--- Arecibo.py
+++ Arecibo.py
@@ -1,12 +1,20 @@
+# Ideas: Add a scoring system, apply a sci-fi theme. This is a message coming from alien invaders that the player 
+# needs to decode. 
+
 import random
 
 message_length = 3
-guesses_allowed = 10
+guesses_remaining = 10
 
 def main():
     print('The program is running.')
     secret_message = generateSecretMessage()
     print('The secret message is {}.'.format(secret_message))
+
+    while guesses_remaining > 0:
+        print('Decryption attempt #{}'.format(guesses_remaining))
+        guess = input('> ')
+        guesses_remaining -= 1
 
 def generateSecretMessage():
     numbers = list('1234567890')
